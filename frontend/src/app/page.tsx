74d340810567dba383859e8bea81292ca9011523
jithingeorgedev <120901504+JithinGeorge10@users.noreply.github.com>
2025-04-10 17:31:24 +0530

    seat booking done

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,103 +1,140 @@
-import Image from "next/image";
+'use client'
+import React, { useState } from 'react';
 
-export default function Home() {
+const ROW_SIZE = 7;
+const TOTAL_SEATS = 80;
+
+
+const TicketBooking: React.FC = () => {
+  const [bookedSeats, setBookedSeats] = useState<number[]>([]);
+  const [numSeatsToBook, setNumSeatsToBook] = useState<number>(0);
+  const [bookedSeatsPreview, setBookedSeatsPreview] = useState<number[]>([]);
+
+  const handleBooking = (): void => {
+    if (bookedSeats.length + numSeatsToBook > TOTAL_SEATS) return;
+
+    const isSeatBooked = (seat: number): boolean => bookedSeats.includes(seat);
+    let newBookings: number[] = [];
+
+
+    for (let rowStart = 1; rowStart <= TOTAL_SEATS; rowStart += ROW_SIZE) {
+      const rowEnd = Math.min(rowStart + ROW_SIZE - 1, TOTAL_SEATS);
+
+      for (let i = rowStart; i <= rowEnd - numSeatsToBook + 1; i++) {
+        const block: number[] = Array.from({ length: numSeatsToBook }, (_, idx) => i + idx);
+
+        const isBlockAvailable = block.every((seat) => !isSeatBooked(seat));
+        if (isBlockAvailable) {
+          newBookings = block;
+          break;
+        }
+      }
+
+      if (newBookings.length > 0) break;
+    }
+
+    if (newBookings.length === 0) {
+      const availableSeats: number[] = Array.from({ length: TOTAL_SEATS }, (_, i) => i + 1).filter(
+        (seat) => !isSeatBooked(seat)
+      );
+      newBookings = availableSeats.slice(0, numSeatsToBook);
+    }
+
+    setBookedSeats((prevSeats) => [...prevSeats, ...newBookings])
+    setBookedSeatsPreview(newBookings)
+  };
+
+  const handleReset = () => {
+    setBookedSeats([]);
+    setBookedSeatsPreview([])
+    setNumSeatsToBook(0);
+  };
+  const handleSeatChange = (e: any) => {
+    console.log(e.target.value)
+    if (e.target.value > 0) {
+      setNumSeatsToBook(Number(e.target.value))
+    }
+  }
+  console.log(bookedSeatsPreview)
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 ">
+      <h1 className="text-3xl text-black font-bold ">Ticket Booking</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="flex flex-col md:flex-row gap-100">
+
+        <div className="grid grid-cols-7 gap-2">
+
+          {Array.from({ length: TOTAL_SEATS }, (_, index) => {
+            const seatNumber = index + 1;
+            const isBooked = bookedSeats.includes(seatNumber);
+            return (
+              <div
+                key={seatNumber}
+                className={`w-10 h-10 flex items-center justify-center rounded-md font-semibold text-black ${isBooked ? 'bg-yellow-500' : 'bg-green-500'
+                  }`}
+              >
+                {seatNumber}
+              </div>
+            );
+          })}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+
+        <div className="flex flex-col  gap-4 justify-center">
+          <div>
+            <label className="text-lg text-black font-bold">Book Seats</label>
+            {bookedSeatsPreview?.length > 0 && (
+              <div className="p-3 rounded-md flex flex-wrap gap-2">
+                {bookedSeatsPreview.map((val, ind) => (
+                  <div
+                    key={ind}
+                    className="bg-yellow-500 text-black px-3 py-1 rounded shadow-sm font-semibold"
+                  >
+                    {val}
+                  </div>
+                ))}
+              </div>
+            )}
+
+
+          </div>
+
+          <div>
+
+            <input
+
+              className="border border-blue-600 p-2 text-black rounded-md min-w-96"
+              value={numSeatsToBook}
+              min={1}
+              max={TOTAL_SEATS - bookedSeats.length}
+              onChange={handleSeatChange}
+
+            />
+            <button
+              className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded"
+              onClick={handleBooking}
+            >
+              Book
+            </button>
+          </div>
+
+          <button
+            className="bg-blue-600  text-white font-semibold py-2 px-4 rounded"
+            onClick={handleReset}
+          >
+            Reset Booking
+          </button>
+        </div>
+      </div>
+
+      <div className="mt-6 flex gap-6 text-lg">
+        <span className="bg-yellow-400 text-black px-3 py-1 rounded">Booked Seats = {bookedSeats.length}</span>
+        <span className="bg-green-400 text-black px-3 py-1 rounded">
+          Available Seats = {TOTAL_SEATS - bookedSeats.length}
+        </span>
+      </div>
     </div>
   );
-}
+};
+
+export default TicketBooking;
