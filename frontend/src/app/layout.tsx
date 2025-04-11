4f39f73eda8d81b918432bab211289dcadc6e0d7
jithingeorgedev <120901504+JithinGeorge10@users.noreply.github.com>
2025-04-11 19:42:02 +0530

    production bug fix

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { StoreProvider } from "./storeProvider";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 import { Toaster } from 'react-hot-toast';
 import 'react-toastify/dist/ReactToastify.css';
