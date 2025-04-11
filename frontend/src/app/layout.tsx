ff9c02696b04dcfe9060b1d49eaea47c25b51f5a
jithingeorgedev <120901504+JithinGeorge10@users.noreply.github.com>
2025-04-11 19:34:39 +0530

    remove unwanted code

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,15 +4,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from 'react-hot-toast';
 import 'react-toastify/dist/ReactToastify.css';
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
